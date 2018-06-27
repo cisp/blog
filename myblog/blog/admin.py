ffe26d57118b9720cbc0002233a375ca131bb053
陈然 <chenran@chenrandeMacBook-Pro.local>
2018-06-28 00:25:16 +0800

    v1.1

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -7,7 +7,7 @@ class BlogArticlesAdmin(admin.ModelAdmin):
     list_filter = ("publish", "author")
     search_fields = ("title", "body")
     raw_id_fields = ("author",)
-    #date_hierarchy = "publish"
-    #ordering = ['publish', 'author']
+    date_hierarchy = "publish"
+    ordering = ['publish', 'author']
 
 admin.site.register(BlogArticles, BlogArticlesAdmin)
